18b70f23e1149af58fcc109fbb6983b37c1321cd
Uriel Milgron <urielmilgron18@gmail.com>
2024-05-28 11:44:05 -0300

    gitignore added

--- ls-advanced.js
+++ ls-advanced.js
@@ -1,8 +1,9 @@
-//When we wants knowns how many files have a directory we can use readdir
+// When we wants knowns how many files have a directory we can use readdir
 const fs = require('node:fs/promises')
 const path = require('node:path')
+const picocolors = require('picocolors')
 
-//When we execute ls-advanced.js we can add other directory and this variable will read the 2st argument
+// When we execute ls-advanced.js we can add other directory and this variable will read the 2st argument
 const systemDir = process.argv[2] ?? '.'
 
 // fs.readdir(systemDir, (err, files) => {
@@ -19,38 +20,38 @@ const systemDir = process.argv[2] ?? '.'
 //     })
 // })
 
-//Code of midu
+// Code of midu
 
-async function ls (folder){
-    let files
-try{
+async function ls (folder) {
+  let files
+  try {
     files = await fs.readdir(folder)
-} catch{
+  } catch {
     console.error("There was an error but I won't tell you what error.")
     process.exit(1)
-}
-const filePromises = files.map(async file => { //This map make a paralell promises, and show this all at time
+  }
+  const filePromises = files.map(async file => { // This map make a paralell promises, and show this all at time
     const filePath = path.join(folder, file)
     let stats
     try {
-        stats = await fs.stat(filePath)
+      stats = await fs.stat(filePath)
     } catch {
-        console.error('Could not be read file')
-        process.exit(1)
+      console.error('Could not be read file')
+      process.exit(1)
     }
     const isDirectory = stats.isDirectory
-    const fileType = isDirectory ? 'Yes, it is a directory' : 'No bro, next, is another type'
+    const fileType = isDirectory ? picocolors.green('Yes, it is a directory') : picocolors.red('No bro, next, is another type')
     const fileSize = stats.size
     const fileModified = stats.mtime.toLocaleDateString()
 
     return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`
-})
+  })
 
-const filesInfo = await Promise.all(filePromises)
+  const filesInfo = await Promise.all(filePromises)
 
-filesInfo.forEach(fileInfo => {
+  filesInfo.forEach(fileInfo => {
     console.log(fileInfo)
-})
+  })
 }
 
 ls(systemDir)
